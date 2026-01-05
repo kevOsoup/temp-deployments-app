8528bc93e118c8f33931cdbbe2ba556ac1facd14
Kev Schwartz <kev.e.schwartz@gmail.com>
2026-01-05 14:27:45 -0500

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rvmp-deployment-app-temp/', // Change this to match your GitHub repo name
 });
