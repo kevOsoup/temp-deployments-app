68fb373e318635c1b365eb182d8cf317fa736a3b
Kev Schwartz <kev.e.schwartz@gmail.com>
2026-01-05 14:34:20 -0500

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rvmp-deployment-app-temp/', // Change this to match your GitHub repo name
+  base: '/temp-deployments-app/', // Change this to match your GitHub repo name
 });
